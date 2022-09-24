2dd60b4ca762e7298dccc46ecfe68c0af7166a1f
Eduardo Rocha <eduardo.rocha2502@homail.com>
2022-09-24 08:46:17 -0300

    página home modificada

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -9,42 +9,42 @@ export const Home = () => {
                 <div className="opacity">
                     <div className="container">
                         <div className='info'>
-                            <div className='info_title'>Conheça os principais filmes</div>
+                            <div className='info_title'>Know the main films</div>
                             <div className='info_bar'></div>
                             <div className='info_desc'>
-                                Um guia sobre os principais filmes da franquia
+                                A guide to the main films of the franchise
                             </div>
-                            <Link to='/movies'><Button text='Go to movies'/></Link>
                         </div>
                     </div>
+                    <Link to='/movies'><Button text='Go to movies'/></Link>
                 </div>  
             </section>
             <section className="section_characteres">
                 <div className="opacity">
                     <div className="container">
                         <div className='info'>
-                            <div className='info_title'>Conheça os principais personagens</div>
+                            <div className='info_title'>Know the main characters</div>
                             <div className='info_bar'></div>
                             <div className='info_desc'>
-                                Um guia sobre os principais personagens da  franquia
+                                A guide to the main characteres of the franchise
                             </div>
-                            <Link to='/characteres'><Button text='Go to characters'/></Link>
                         </div>
                     </div>
+                    <Link to='/characteres'><Button text='Go to characters'/></Link>
                 </div>  
             </section>
             <section className="section_veiculos">
                 <div className="opacity">
                     <div className="container">
                         <div className='info'>
-                            <div className='info_title'>Conheça os principais veículos</div>
+                            <div className='info_title'>Know the main vehicles</div>
                             <div className='info_bar'></div>
                             <div className='info_desc'>
-                                Um guia sobre os principais veículos da  franquia
+                                A guide to the main vehicles of the franchise
                             </div>
-                            <Link to='/vehicles'><Button text='Go to vehicles'/></Link>
                         </div>
                     </div>
+                    <Link to='/vehicles'><Button text='Go to vehicles'/></Link>
                 </div>  
             </section>
         </div>
